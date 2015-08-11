c448d7fdaea9a0abdeec966d9ca2de38c3026801
pixelshade <pixelshade@Pejkos-MacBook-Pro.local>
2015-08-11 19:58:36 +0200

    upload to imgur

---
+++ app/src/main/java/pixelshade/geniusloci/Constants.java
@@ -0,0 +1,33 @@
+package pixelshade.geniusloci;
+
+/**
+ * Created by AKiniyalocts on 2/23/15.
+ */
+public class Constants {
+    /*
+      Logging flag
+     */
+    public static final boolean LOGGING = false;
+
+    /*
+      Your imgur client id. You need this to upload to imgur.
+
+      More here: https://api.imgur.com/
+     */
+    // TODO ADD CLIENT ID AND SECRET
+    public static final String MY_IMGUR_CLIENT_ID = "";
+    public static final String MY_IMGUR_CLIENT_SECRET = "";
+
+    /*
+      Redirect URL for android.
+     */
+    public static final String MY_IMGUR_REDIRECT_URL = "http://android";
+
+    /*
+      Client Auth
+     */
+    public static String getClientAuth() {
+        return "Client-ID " + MY_IMGUR_CLIENT_ID;
+    }
+
+}
