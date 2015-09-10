f7c1276a4897e946e21c572f7b5b77686366e024
pixelshade <psuldo@gmail.com>
2015-09-11 01:34:04 +0200

    server response when GetNear

--- app/src/main/java/pixelshade/geniusloci/model/ServerAPI.java
+++ app/src/main/java/pixelshade/geniusloci/model/ServerAPI.java
@@ -42,6 +42,6 @@ public interface ServerAPI {
     void getNear(
             @Query("longitude") double longitude,
             @Query("latitude") double latitutde,
-            Callback<ServerListGhostsResponse> cb
+            Callback<List<GhostEntry>> cb
     );
 }
