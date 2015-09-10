b4669918606564ce5c2d149356bcb76426b3180b
pixelshade <psuldo@gmail.com>
2015-09-10 17:23:08 +0200

    server api getAll entries

---
+++ app/src/main/java/pixelshade/geniusloci/model/ServerListGhostsResponse.java
@@ -0,0 +1,10 @@
+package pixelshade.geniusloci.model;
+
+import java.util.List;
+
+/**
+ * Created by pixelshade on 10/09/15.
+ */
+public class ServerListGhostsResponse {
+    public List<GhostEntry> ghostEntries;
+}
